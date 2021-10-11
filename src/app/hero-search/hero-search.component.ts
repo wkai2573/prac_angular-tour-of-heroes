1b55397e40bbee0bab4656416a5cdf1939888751
kai <s930212123@gmail.com>
2021-10-11 23:51:21 +0800

    push

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -13,22 +13,25 @@ import {HeroService} from '../hero.service';
 	styleUrls: ['./hero-search.component.scss']
 })
 export class HeroSearchComponent implements OnInit {
-	heroes$!: Observable<Hero[]>;
-	private searchTerms = new Subject<string>();
 
 	constructor(private heroService: HeroService) {}
 
-	// Push a search term into the observable stream.
+	//搜尋英雄列表
+	heroes$!: Observable<Hero[]>;
+
+	//搜尋對象, Subject:可觀察的 & 可訂閱其他觀察對象
+	private searchTerms = new Subject<string>();
+
+	//將搜索詞推送到可觀察流中。
 	search(term: string): void {
 		this.searchTerms.next(term);
 	}
 
 	ngOnInit(): void {
 		this.heroes$ = this.searchTerms.pipe(
-			debounceTime(300), //每次請求至少等待300毫秒
+			debounceTime(300), //停止輸入後至少300ms後才請求
 			distinctUntilChanged(), //與前一項相同則不請求
-
-			// switch to new search observable each time the term changes
+			//每次詞語更改時切換到新的搜索 observable
 			switchMap((term: string) => this.heroService.searchHeroes(term)),
 		);
 	}
